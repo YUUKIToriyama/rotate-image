0a40356ec7635d0a354e598532153fbad49049ab
YUUKIToriyama <github@toriyama.dev>
2022-11-04 00:51:25 +0900

    画像処理の結果をcanvasに出力する関数を追加した

--- src/lib.rs
+++ src/lib.rs
@@ -5,9 +5,10 @@ mod utils;
 
 use image::Image;
 use rotate::rotate_image;
-use wasm_bindgen::{prelude::wasm_bindgen, JsCast};
+use wasm_bindgen::{prelude::wasm_bindgen, Clamped, JsCast, JsValue};
 use web_sys::CanvasRenderingContext2d;
 use web_sys::HtmlCanvasElement;
+use web_sys::ImageData;
 
 #[wasm_bindgen]
 pub struct ImageConverter {
@@ -34,5 +35,23 @@ impl ImageConverter {
 
     pub fn rotate(&mut self, radian: f32) {
         self.image = rotate_image(&self.image, radian);
+    }
+
+    pub fn to_canvas(&self, canvas: &HtmlCanvasElement) -> Result<(), JsValue> {
+        let context = canvas
+            .get_context("2d")
+            .unwrap()
+            .unwrap()
+            .dyn_into::<CanvasRenderingContext2d>()
+            .unwrap();
+        canvas.set_width(self.image.width);
+        canvas.set_height(self.image.height);
+        let image_data = ImageData::new_with_u8_clamped_array_and_sh(
+            Clamped(&self.image.to_u8_vec()),
+            self.image.width,
+            self.image.height,
+        )
+        .unwrap();
+        context.put_image_data(&image_data, 0.0, 0.0)
     }
 }
